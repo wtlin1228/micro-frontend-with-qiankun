2f2403ae35343e9850cda9c5078bd509c71aa2fe
wtlin1228 <wtlin1228@gmail.com>
2025-05-06 07:25:38 +0900

    feat: setup MUI for kirby app

--- apps/kirby/src/App.tsx
+++ apps/kirby/src/App.tsx
@@ -1,8 +1,16 @@
+import { Button, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  cssVariables: true,
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <h1>Kirby</h1>
-    </>
+      <Button variant="contained">Click me</Button>
+    </ThemeProvider>
   );
 }
 
--- apps/kirby/vite.config.ts
+++ apps/kirby/vite.config.ts
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  mode: "development",
   define: {
-    "process.env.NODE_ENV": '"production"', // or "development"
+    "process.env.NODE_ENV": '"development"', // or "development"
     "process.env": "{}", // optional fallback
     process: "{}",
   },
   plugins: [react()],
   build: {
+    minify: false,
     lib: {
       entry: "src/main.tsx",
       formats: ["umd"],
